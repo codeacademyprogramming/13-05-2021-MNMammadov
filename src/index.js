6af36781e89bfedafc3b29a37aef848c6af8357a
MNMammadov <mehdinm@code.edu.az>
2021-05-14 07:24:12 +0400

    Added export and changed package name

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
-const translation = {
-    0: 'sıfır',
-    1: 'bir',
-    2: 'iki',
-    3: 'üç',
-    4: 'dörd',
-    5: 'beş',
-    6: 'altı',
-    7: 'yeddi',
-    8: 'səkkiz',
-    9: 'doqquz',
-    10: 'on',
-    20: 'iyirmi',
-    30: 'otuz',
-    40: 'qırx',
-    50: 'əlli',
-    60: 'altımış',
-    70: 'yetmiş',
-    80: 'səksən',
-    90: 'doxsan',
-    100: 'yüz',
-    1000: 'min',
-    1000000: 'milyon',
-}
+export function numberToWords(num) {
+    const numberToWordObj = {
+        0: 'sıfır',
+        1: 'bir',
+        2: 'iki',
+        3: 'üç',
+        4: 'dörd',
+        5: 'beş',
+        6: 'altı',
+        7: 'yeddi',
+        8: 'səkkiz',
+        9: 'doqquz',
+        10: 'on',
+        20: 'iyirmi',
+        30: 'otuz',
+        40: 'qırx',
+        50: 'əlli',
+        60: 'altımış',
+        70: 'yetmiş',
+        80: 'səksən',
+        90: 'doxsan',
+        100: 'yüz',
+        1000: 'min',
+        1000000: 'milyon',
+    }
 
-
-function numberToWords(num) {
-    if (translation[num]) {
-        return translation[num];
+    if (numberToWordObj[num]) {
+        return numberToWordObj[num];
     }
 
     const length = num.toString().length;
